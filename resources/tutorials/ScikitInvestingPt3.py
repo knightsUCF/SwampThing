f65135fa48e0681fea318a25f4860f9867d963ce
peterknights1 <peterk@knights.ucf.edu>
2017-08-16 20:37:24 -0400

    Update ScikitInvestingPt3.py

--- resources/tutorials/ScikitInvestingPt3.py
+++ resources/tutorials/ScikitInvestingPt3.py
@@ -1,1 +1,15 @@
+# Scikit Machine Learning Tutorial p. 3
+# https://www.youtube.com/watch?v=AleGZ9dkfPs
+
+# helpful overview diagram of scikit learn: http://scikit-learn.org/stable/tutorial/machine_learning_map/index.html
+# based on this using SGD classifier, have more than 50 samples, we are predicting a category, and we have labeled data
+
+# get data from yahoo by stock
+# control + click to open multiple links without leaving main tab
+
+# debt to equity as first example
+# also can use trailing p/e, don't worry about projected p/e
+
+
+
 
